7294e115e09a7e0d73ae64dc551ebe0e873a126e
Chris Mackey <chris@ladybug.tools>
2024-11-15 16:46:16 -0800

    feat(model): Add methods to perform model comparisons

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -4,7 +4,8 @@ import time
 from click.testing import CliRunner
 
 from ladybug.commandutil import run_command_function
-from dragonfly_display.cli import model_to_vis_set_cli, model_to_vis_set
+from dragonfly_display.cli import model_to_vis_set_cli, model_to_vis_set, \
+    model_comparison_to_vis_set_cli, model_comparison_to_vis_set
 
 
 def test_model_to_vis_set_cli():
@@ -43,3 +44,33 @@ def test_model_to_vis_set():
     run_command_function(model_to_vis_set, cmd_args, cmd_options)
     assert os.path.isfile(output_vis)
     os.remove(output_vis)
+
+
+def test_model_comparison_to_vis_set_cli():
+    """Test the model_comparison_to_vis_set function that runs within the CLI."""
+    base_model = './tests/json/base_model.dfjson'
+    incoming_model = './tests/json/incoming_model.dfjson'
+    output_vis = './tests/json/comparison.html'
+    runner = CliRunner()
+    t0 = time.time()
+    cmd_args = [base_model, incoming_model, '--output-format', 'html',
+                '--output-file', output_vis]
+    result = runner.invoke(model_comparison_to_vis_set_cli, cmd_args)
+    run_time = time.time() - t0
+    assert result.exit_code == 0
+    assert run_time < 10
+    assert os.path.isfile(output_vis)
+    os.remove(output_vis)
+
+
+def test_model_comparison_to_vis_set():
+    """Test the model_comparison_to_vis_set function."""
+    base_model = './tests/json/base_model.dfjson'
+    incoming_model = './tests/json/incoming_model.dfjson'
+    cmd_args = [base_model, incoming_model]
+    cmd_options = {'--output-format': 'vtkjs'}
+    vtkjs_str = run_command_function(model_comparison_to_vis_set, cmd_args, cmd_options)
+
+    assert isinstance(vtkjs_str, str)
+    assert len(vtkjs_str) > 1000
+
